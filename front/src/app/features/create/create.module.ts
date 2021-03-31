cf8cf22d3c36d6742a7f5a5ae822f9621395b850
etibulea <eduard.tibuleac@bytex.ro>
2021-03-31 15:05:00 +0300

    feat: added some of the fields on page

--- front/src/app/features/create/create.module.ts
+++ front/src/app/features/create/create.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './pages';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+const MATERIAL_COMPONENTS: any[] = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSlideToggleModule,
+];
 @NgModule({
-  imports: [CommonModule, CreateRoutingModule],
+  imports: [CommonModule, CreateRoutingModule, MATERIAL_COMPONENTS],
   declarations: [CreateComponent],
 })
 export class CreateModule {}
